a94a085e72c5a19b3c5a257a9ded1796c8f70717
Stuart Wheater <stuart.wheater@arjuna.com>
2023-05-27 22:24:47 +0100

    Improved template and template info loading

--- images/lab-devel/src/main/java/com/arjuna/sde/lab/StoresResetResource.java
+++ images/lab-devel/src/main/java/com/arjuna/sde/lab/StoresResetResource.java
@@ -20,14 +20,19 @@ import jakarta.enterprise.context.ApplicationScoped;
 import org.jboss.logging.Logger;
 
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import org.bson.Document;
 
 import io.minio.MinioClient;
+import io.minio.messages.Item;
+import io.minio.Result;
 import io.minio.PutObjectArgs;
 import io.minio.BucketExistsArgs;
 import io.minio.RemoveBucketArgs;
+import io.minio.RemoveObjectArgs;
+import io.minio.ListObjectsArgs;
 import io.minio.MakeBucketArgs;
 
 @jakarta.ws.rs.Path("/stores_reset")
@@ -53,6 +58,7 @@ public class StoresResetResource
         try
         {
             loadTemplates(minioClient, FileSystems.getDefault().getPath("/data/templates"));
+            loadTemplateInfos(mongoClient, FileSystems.getDefault().getPath("/data/template_infos"));
         }
         catch (Error error)
         {
@@ -71,22 +77,40 @@ public class StoresResetResource
     private void loadTemplates(MinioClient minioClient, Path templateFilesPath)
     {
         log.info("#### loadTemplates");
-        log.info(templateFilesPath.getFileName());
 
-        try (DirectoryStream<Path> templatePathsStream = Files.newDirectoryStream(templateFilesPath))
+        try
         {
-            for (Path templatePath: templatePathsStream)
+            if (minioClient.bucketExists(BucketExistsArgs.builder().bucket("templates").build()))
             {
-                log.info(templatePath.getFileName());
+                  Iterable<Result<Item>> bucketObjects = minioClient.listObjects(ListObjectsArgs.builder().bucket("templates").build());
+                  for (Result<Item> bucketObject: bucketObjects)
+                  {
+                      log.infof("Template File name: %s", bucketObject.get().objectName());
+                      minioClient.removeObject(RemoveObjectArgs.builder().bucket("templates").object(bucketObject.get().objectName()).build());
+                  }
 
-                if (minioClient.bucketExists(BucketExistsArgs.builder().bucket("templates").build()))
-                    minioClient.removeBucket(RemoveBucketArgs.builder().bucket("templates").build());
+                  minioClient.removeBucket(RemoveBucketArgs.builder().bucket("templates").build());
+            }
+            minioClient.makeBucket(MakeBucketArgs.builder().bucket("templates").build());
 
-                minioClient.makeBucket(MakeBucketArgs.builder().bucket("templates").build());
+            try (DirectoryStream<Path> templatePathsStream = Files.newDirectoryStream(templateFilesPath))
+            {
+                for (Path templatePath: templatePathsStream)
+                {
+                    log.info(templatePath.getFileName());
 
-                InputStream inputStream = new FileInputStream(templatePath.toFile());
-                minioClient.putObject(PutObjectArgs.builder().bucket("templates").object(templatePath.toString()).stream(inputStream, -1, 10485760).contentType(MediaType.APPLICATION_JSON).build());
-                inputStream.close();
+                    InputStream inputStream = new FileInputStream(templatePath.toFile());
+                    minioClient.putObject(PutObjectArgs.builder().bucket("templates").object(templatePath.toString()).stream(inputStream, -1, 10485760).contentType(MediaType.APPLICATION_JSON).build());
+                    inputStream.close();
+                }
+            }
+            catch (Error error)
+            {
+                throw error;
+            }
+            catch (Exception exception)
+            {
+                throw exception;
             }
         }
         catch (Error error)
@@ -99,5 +123,45 @@ public class StoresResetResource
         }
 
         log.info("#### loadTemplates - end");
+    }
+
+    private void loadTemplateInfos(MongoClient mongoClient, Path templateInfoFilesPath)
+    {
+        log.info("#### loadTemplateInfos");
+
+        try
+        {
+            MongoDatabase sde = mongoClient.getDatabase("sde");
+            sde.getCollection("template_infos").drop();
+
+            try (DirectoryStream<Path> templateInfoPathsStream = Files.newDirectoryStream(templateInfoFilesPath))
+            {
+                for (Path templateInfoPath: templateInfoPathsStream)
+                {
+                    log.infof("Template Info File name: %s", templateInfoPath.getFileName());
+
+                    InputStream inputStream = new FileInputStream(templateInfoPath.toFile());
+                    inputStream.close();
+                }
+            }
+            catch (Error error)
+            {
+                throw error;
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+        catch (Error error)
+        {
+            log.error("Error while loading TemplateInfos", error);
+        }
+        catch (Exception exception)
+        {
+            log.error("Exception while loading TemplateInfos", exception);
+        }
+
+        log.info("#### loadTemplateInfos - end");
     }
 }
