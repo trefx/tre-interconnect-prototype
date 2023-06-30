40f5c3af17c7c0ea85696610dd2347c7bf93922e
Stuart Wheater <stuart.wheater@arjuna.com>
2023-06-30 14:27:25 +0100

    Improve webportal

---
+++ images/lab-webportal/src/app/provider-list/provider-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component
+({
+    selector:    'sde-provider-list',
+    templateUrl: './provider-list.component.html',
+    styleUrls:   ['./provider-list.component.scss']
+})
+export class ProviderListComponent
+{
+}
